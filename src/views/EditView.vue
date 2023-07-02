736c7f176b84df944f8b319279b8f9974dcde898
米川貴也 <zuomujianwu1@gmail.com>
2023-07-02 16:31:56 +0900

    memo.title

---
+++ src/views/EditView.vue
@@ -0,0 +1,17 @@
+<template>
+    {{memo.title}}
+</template>
+
+
+<script>
+export default {
+    name: 'EditView',
+    computed {
+        memo(){
+            const id = parseInt(this.$route.params.id)
+            return this.$store.getters.getMemoById(id)
+        }
+    }
+}
+
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,6 +9,28 @@
   </div>
 </template>
 
+<style scoped>
+ul {
+  margin: 0;
+  padding: 0;
+}
+
+li {
+  list-style: none;
+  border-bottom: 1px solid #ccc;
+  padding-bottom: 10px;
+  margin-bottom: 10px;
+}
+
+li a {
+  color: #999;
+  text-decoration: none;
+  width: 100%;
+  display: block;
+}
+
+</style>
+
 <script>
   export default{
     name: 'HomeView',
